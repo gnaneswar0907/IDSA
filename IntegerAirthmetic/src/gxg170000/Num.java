cb339d475531cad148574d381961b0df45ce3cd7
gnaneswar0907 <asish.gnan@gmail.com>
2019-09-16 20:58:11 -0500

    restarted Integer Airthmetic

--- IntegerAirthmetic/src/gxg170000/Num.java
+++ IntegerAirthmetic/src/gxg170000/Num.java
@@ -1,7 +1,7 @@
 package gxg170000;
 
 public class Num {
-    static long defaultBase = 10;  // Change as needed
+    static long defaultBase = 1000;  // Change as needed
     long base = 1000;  // Change as needed
     long[] arr;  // array to store arbitrarily large integers
     boolean isNegative;  // boolean flag to represent negative numbers
@@ -9,10 +9,10 @@ public class Num {
 
     public Num(String s) {
         int count = 0;
-
-        while(base>1){
+        long temp = base;
+        while(temp>1){
             count++;
-            base = base/10;
+            temp = temp/10;
         }
 
         String reversedString = reverse(s);
@@ -27,11 +27,10 @@ public class Num {
             j++;
         }
 
-
-
+        len = arr.length;
     }
 
-    public String reverse(String s){
+    public static String reverse(String s){
         StringBuilder sb = new StringBuilder();
         sb.append(s);
         return sb.reverse().toString();
@@ -42,15 +41,89 @@ public class Num {
     }
 
     public static Num add(Num a, Num b) {
-        return null;
+        int l1 = a.len;
+        int l2 = b.len;
+        long temp = 0;
+        long[] res = new long[Math.max(l1,l2)+1];
+        for(int i=0;i<Math.max(l1,l2);i++){
+            if(i<Math.min(l1,l2)){
+                temp = temp + a.arr[i]+b.arr[i];
+            }
+            else{
+                if(i>=l1){
+                    temp = temp + b.arr[i];
+                }
+                else{
+                    temp = temp + a.arr[i];
+                }
+            }
+            if(temp>defaultBase-1){
+                res[i] = temp - defaultBase;//Change 1000 to base
+                temp = 1;
+            }
+            else{
+                res[i] = temp;
+                temp = 0;
+            }
+        }
+
+        long result = 0;
+
+        for(int i=0;i<res.length;i++){
+            result = result + (res[i]* ((long)(Math.pow(a.base,i)))); // might not work for large numbers
+        }
+        return new Num(Long.toString(result));
     }
 
     public static Num subtract(Num a, Num b) {
+//        int l1 = a.len;
+//        int l2 = b.len;
+//        long temp = 0;
+//        long[] res = new long[Math.max(l1,l2)];
+//        for(int i=0;i<Math.max(l1,l2);i++){
+//            if(a.arr[i]<b.arr[i]){
+//                for(int j=i-1; j>0;j--){
+//
+//                }
+//            }
+//            else {
+//                res[i] = a.arr[i] - b.arr[i];
+//                temp = 0;
+//            }
+//        }
+//
+//        System.out.print("over");
         return null;
     }
 
     public static Num product(Num a, Num b) {
-        return null;
+        int l1 = a.len;
+        int l2 = b.len;
+        int l3 = Math.max(l1,l2);
+        int l4 = Math.min(l1,l2);
+        long temp = 0;
+        long multiSum = 0;
+        String multi = "";
+        for(int i=0;i<l3;i++){
+            for(int j=0;j<l4;j++){
+                long sum1 = 0;
+                if(l3 == l2){
+                    sum1 = (a.arr[j]*b.arr[i]) + temp;
+                }
+                else{
+                    sum1 = (b.arr[j]*a.arr[i]) + temp;
+                }
+                if(j<l4-1){
+                    temp = sum1/defaultBase; // change 1000 to base
+                    sum1 = sum1%defaultBase; // change 1000 to base
+                }
+                multi = sum1 + multi;
+            }
+            temp=0;
+            multiSum = multiSum + ((Long.parseLong(multi) ) * (long) Math.pow(defaultBase,(double) i));
+            multi = "";
+        }
+        return new Num(Long.toString(multiSum));
     }
 
     // Use divide and conquer
@@ -84,6 +157,13 @@ public class Num {
     // For example, if base=100, and the number stored corresponds to 10965,
     // then the output is "100: 65 9 1"
     public void printList() {
+        String s ="";
+        System.out.print(base +": ");
+        for(long i:arr){
+//            System.out.print(i+s);
+            s = i +s;
+        }
+        System.out.print(s);
     }
 
     // Return number to a string in base 10
@@ -100,7 +180,23 @@ public class Num {
 
     // Divide by 2, for using in binary search
     public Num by2() {
-        return null;
+        long temp = 0;
+        long[] res = new long[len];
+        int j = 0;
+        for(int i=len-1;i>=0;i--){
+            res[j] = (arr[i] + (temp * (long)(Math.pow(1000, (double)(i+1)))))/2;
+            temp = arr[i]%2;
+            j++;
+        }
+        temp = 0;
+        j=0;
+        for(int i=res.length-1;i>=0;i--){
+            if(res[i]!=0){
+                temp = temp + (res[i]* (long)(Math.pow(1000, (long)j)));
+                j++;
+            }
+        }
+        return new Num(Long.toString(temp));
     }
 
     // Evaluate an expression in postfix and return resulting number
@@ -119,12 +215,21 @@ public class Num {
 
 
     public static void main(String[] args) {
-//        Num x = new Num(999);
-        Num y = new Num("4000028");
-//        Num z = Num.add(x, y);
+        Num x = new Num("12345678912123456");
+        Num y = new Num("12345678912123456");
+//        System.out.println(1313%2139);
+//        x.by2();
+//        System.out.print(Math.pow(2, 31));
+//        Num sum = Num.product(x,y);
+//        sum.printList();
+//        long result = 0;
+//
+
+//        System.out.print("oo");
+        Num z = Num.product(x, y);
 //        System.out.println(z);
 //        Num a = Num.power(x, 8);
 //        System.out.println(a);
-//        if(z != null) z.printList();
+        if(z != null) z.printList();
     }
 }
