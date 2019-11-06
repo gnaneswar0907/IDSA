89776f084fc3d8cb451def5a3135b5c5deffc492
gnaneswar0907 <asish.gnan@gmail.com>
2019-11-06 13:56:05 -0600

    insert method completed for MDS

--- MultiDimensionalSearch/src/gxg170000/MDS.java
+++ MultiDimensionalSearch/src/gxg170000/MDS.java
@@ -1,20 +1,61 @@
-/** Starter code for MDS
- *  @author rbk
- */
+/** @authors
+ * Meetika Sharma - mxs173530
+ * Ch Muhammad Talal Muneer - cxm180004
+ * Chirag Shahi - cxs180005
+ * Gnaneswar Gandu - gxg170000
+ * Multi Dimensional Search
+ **/
 
 // Change to your net id
-package idsa;
+package gxg170000;
+
+import java.util.*;
 
 // If you want to create additional classes, place them in this file as subclasses of MDS
 
 public class MDS {
     // Add fields of MDS here
 
-    // Constructors
-    public MDS() {
+    private TreeMap<Long, Item> treeMap;
+    private HashMap<Long, TreeSet<Item>> table;
+    private long size;
+
+    // Information about each Item included in Item Class
+    static class Item{
+        private Long id;
+        private HashSet<Long> description; // used HashSet for distinct descriptions
+        private Money price;
+
+        public Item(long id, Money price, List<Long> list){
+            this.id = id;
+            this.price = price;
+            this.description = new HashSet<Long>();
+            for(Long l : list){
+                this.description.add(l);
+            }
+        }
+
+        public long getId(){
+            return this.id;
+        }
+
+        public int compareTo(Item item){
+            return this.id.compareTo(item.id);
+        }
+
+        public String toString(){
+            return "[ id : " + id + ",\t price : " + price.toString() + ",\t description : " + description.toString() + "]";
+        }
     }
 
-    /* Public methods of MDS. Do not change their signatures.
+    // Constructors
+    public MDS() {
+        this.size = 0;
+        this.treeMap = new TreeMap<>();
+        this.table = new HashMap<>();
+    }
+
+    /** Public methods of MDS. Do not change their signatures.
        __________________________________________________________________
        a. Insert(id,price,list): insert a new item whose description is given
        in the list.  If an entry with the same id already exists, then its
@@ -22,13 +63,65 @@ public class MDS {
        is null or empty, in which case, just the price is updated. 
        Returns 1 if the item is new, and 0 otherwise.
     */
-    public int insert(long id, Money price, java.util.List<Long> list) {
-	return 0;
+    public int insert(long id, Money price, List<Long> list) {
+        if(treeMap.containsKey(id)){
+            if(!(list==null || list.isEmpty())){
+                deleteInTable(id);
+                treeMap.get(id).description.clear();
+                for(Long dd : list){
+                    treeMap.get(id).description.add(dd);
+                }
+                insertInTable(id);
+            }
+            treeMap.get(id).price = price;
+            return 0;
+        }
+        Item newItem = new Item(id,price,list);
+        treeMap.put(id, newItem);
+        insertInTable(id);
+        size++;
+        return 1;
+    }
+
+    /**
+     * Helper method: Inserts descriptions of the Item(id) in table and
+     * adds mapping from that description to the HashSet
+     * updated with the Item(id), if needed.
+     */
+    private void insertInTable(Long id){
+        Item item = treeMap.get(id);
+        for(Long l: item.description){
+            if(table.containsKey(l)){
+                table.get(l).add(item); // adding/replacing the item
+            }
+            else{
+                TreeSet<Item> newSet = new TreeSet<>();
+                newSet.add(item);      // adding first element in TreeSet
+                table.put(l, newSet); //inserting the map from l to new TreeSet
+            }
+        }
+    }
+
+    /**
+     * Helper method: To remove all the descriptions from table
+     * whatever existing descriptions the Item(id) has.
+     * Post Condition: need to perform descriptions.clear() from treeMap
+     */
+    private void deleteInTable(Long id){
+        Item item = treeMap.get(id);
+        for(Long l : item.description){
+            table.get(l).remove(item);
+            if(table.get(l).isEmpty()){   // when the TreeSet is emptied
+                table.remove(l);
+            }
+        }
     }
 
     // b. Find(id): return price of item with given id (or 0, if not found).
     public Money find(long id) {
-	return new Money();
+        Item item = treeMap.get(id);
+        if(item==null) return new Money();
+	    return item.price;
     }
 
     /* 
